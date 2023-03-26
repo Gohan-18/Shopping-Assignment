4d0660bd9a915adc9862c18c6675fbdb761c3418
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-26 19:39:33 +0530

    final ui touchup

--- Sh-assign/src/components/Cart.jsx
+++ Sh-assign/src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@emotion/react";
 import {
   Box,
+  Button,
   Card,
   CardMedia,
   Container,
@@ -14,12 +15,14 @@ import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import RemoveRoundedIcon from "@mui/icons-material/RemoveRounded";
 import { getSubTotal } from "../utils";
 import { addTOCart, removeFromCart } from "../feature/Cart-slice";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const theme = useTheme();
   const { cartItems } = useSelector((state) => state?.cart);
   const subTotal = getSubTotal(cartItems)?.toFixed(2);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function increaseItem({ product, quantity }) {
     dispatch(addTOCart({ product, quantity }));
@@ -50,92 +53,114 @@ const Cart = () => {
           CART
         </Typography>
         {/* </Box> */}
-        <Box sx={{ width: "100%", py: "20px" }}>
-          {cartItems.map((item) => {
-            const { product, quantity } = item;
-            return (
-              <Card
-                key={product.id}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "start",
-                  px: {
-                    xs: "10px",
-                    md: "30px",
-                  },
-                  my: "30px",
-                  py: "20px",
-                }}
-              >
-                <Box sx={{ px: "10px" }}>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    alt={product.title}
-                    sx={{
-                      //   alignSelf: "center",
-                      width: theme.spacing(20),
-                      height: theme.spacing(20),
-                      objectFit: "contain",
-                      py: theme.spacing(2),
-                    }}
-                  />
-                </Box>
-                <Box>
-                  <Typography
-                    sx={{
-                      fontSize: "20px",
-                      fontWeight: 500,
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      display: "-webkit-box",
-                      WebkitLineClamp: "2",
-                      WebkitBoxOrient: "vertical",
-                    }}
-                  >
-                    {product.title}{" "}
-                  </Typography>
-                  <Rating
-                    readOnly
-                    precision={0.5}
-                    value={product.rating.rate}
-                    size="small"
-                  />
-                  <Typography sx={{ color: "#6c757d", fontWeight: "400" }}>
-                    Quantity
-                  </Typography>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "start",
-                      gap: 1,
-                    }}
-                  >
-                    <IconButton onClick={() => decreaseItem({ product })}>
-                      <RemoveRoundedIcon sx={{ fontSize: "20px" }} />
-                    </IconButton>
-                    <Typography sx={{ color: "#6c757d", fontWeight: "400" }}>
-                      {quantity}
-                    </Typography>
-                    <IconButton
-                      onClick={() => increaseItem({ product, quantity })}
-                    >
-                      <AddRoundedIcon sx={{ fontSize: "20px" }} />
-                    </IconButton>
+        {cartItems.length ? (
+          <Box sx={{ width: "100%", py: "20px" }}>
+            {cartItems.map((item) => {
+              const { product, quantity } = item;
+              return (
+                <Card
+                  key={product.id}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "start",
+                    px: {
+                      xs: "10px",
+                      md: "30px",
+                    },
+                    my: "30px",
+                    py: "20px",
+                  }}
+                >
+                  <Box sx={{ px: "10px" }}>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      alt={product.title}
+                      sx={{
+                        //   alignSelf: "center",
+                        width: theme.spacing(20),
+                        height: theme.spacing(20),
+                        objectFit: "contain",
+                        py: theme.spacing(2),
+                      }}
+                    />
                   </Box>
-                  <Typography sx={{fontSize: {
-                    xs: '16px',
-                    sm: '20px'
-                  }}} >
-                    Total : ${getSubTotal([{ product, quantity }]).toFixed(2)}
-                  </Typography>
-                </Box>
-              </Card>
-            );
-          })}
-        </Box>
+                  <Box>
+                    <Typography
+                      sx={{
+                        fontSize: "20px",
+                        fontWeight: 500,
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        display: "-webkit-box",
+                        WebkitLineClamp: "2",
+                        WebkitBoxOrient: "vertical",
+                      }}
+                    >
+                      {product.title}{" "}
+                    </Typography>
+                    <Rating
+                      readOnly
+                      precision={0.5}
+                      value={product.rating.rate}
+                      size="small"
+                    />
+                    <Typography sx={{ color: "#6c757d", fontWeight: "400" }}>
+                      Quantity
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "start",
+                        gap: 1,
+                      }}
+                    >
+                      <IconButton onClick={() => decreaseItem({ product })}>
+                        <RemoveRoundedIcon sx={{ fontSize: "20px" }} />
+                      </IconButton>
+                      <Typography sx={{ color: "#6c757d", fontWeight: "400" }}>
+                        {quantity}
+                      </Typography>
+                      <IconButton
+                        onClick={() => increaseItem({ product, quantity })}
+                      >
+                        <AddRoundedIcon sx={{ fontSize: "20px" }} />
+                      </IconButton>
+                    </Box>
+                    <Typography
+                      sx={{
+                        fontSize: {
+                          xs: "16px",
+                          sm: "20px",
+                        },
+                      }}
+                    >
+                      Total : ${getSubTotal([{ product, quantity }]).toFixed(2)}
+                    </Typography>
+                  </Box>
+                </Card>
+              );
+            })}
+          </Box>
+        ) : (
+          <Container maxWidth='sm' sx={{display: 'flex', width: '100%', justiftContent: 'center', flexDirection: 'column'}} >
+            <Typography
+              sx={{
+                fontSize: {
+                  xs: "20px",
+                  sm: "40px",
+                },
+                fontWeight: 600,
+                color: "#6c757d",
+                textAlign: "center",
+                py: "30px",
+              }}
+            >{`CART EMPTY :(`}</Typography>
+            <Button onClick={() => navigate('/')} variant='contained' >ADD NOW</Button>
+          </Container>
+        )}
 
         <Card
           sx={{
